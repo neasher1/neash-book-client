b11e096a6ae2ca2871f888562a13102fcaba5a2b
neasher <neasher75@gmail.com>
2022-12-29 01:54:31 +0600

    max width

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import router from './Routes/Router';
 
 function App() {
   return (
-    <div>
+    <div className="max-w-[1200px] mx-auto">
       <RouterProvider router={router}></RouterProvider>
       <Toaster />
     </div>
