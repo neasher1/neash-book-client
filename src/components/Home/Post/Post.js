ec25a5af8c132f08831f0485bbb65a3359626aed
neasher <neasher75@gmail.com>
2022-12-30 00:07:50 +0600

    Add post done

--- src/components/Home/Post/Post.js
+++ src/components/Home/Post/Post.js
@@ -1,18 +1,81 @@
-import React from 'react';
-import img from '../../../Assets/neasher-bg1.png';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider';
 
 const Post = () => {
+    const { user } = useContext(AuthContext);
+    const imgbbHostKey = process.env.REACT_APP_imgbb_key;
+    const navigate = useNavigate();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const handleAddPost = (data) => {
+        const image = data.image[0];
+        const formData = new FormData();
+        formData.append('image', image);
+
+        const url = `https://api.imgbb.com/1/upload?key=${imgbbHostKey}`;
+        fetch(url, {
+            method: 'POST',
+            body: formData,
+        })
+            .then(res => res.json())
+            .then(imgData => {
+                if (imgData.success) {
+
+                    const addPost = {
+                        img: imgData.data.url,
+                        details: data.postDetails
+                    }
+
+                    fetch('http://localhost:5000/posts', {
+                        method: 'POST',
+                        headers: {
+                            'content-type': 'application/json',
+                        },
+                        body: JSON.stringify(addPost)
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            toast.success('Successfully Posted');
+                            navigate('/media');
+                        })
+
+                }
+            })
+
+    }
+
     return (
-        <div className='border-solid border-2 p-6 bg-white shadow-sm my-8 rounded-md border-t-0 mx-4'>
-            <div className='flex flex-row justify-center items-center gap-4'>
-                <img className="mask mask-circle w-12" src={img} alt='user' />
-                <input type="text" placeholder="What's on your mind?" className="input input-bordered w-full max-w-xs" />
-            </div>
-            <div className="divider"></div>
-            <div className='flex flex-row justify-center items-center gap-4'>
-                <input type="file" className="file-input file-input-bordered file-input-primary w-64" />
-                <button className='btn btn-primary text-white'>Post</button>
-            </div>
+        <div>
+            {
+                user &&
+                <form onSubmit={handleSubmit(handleAddPost)}>
+                    <div className='border-solid border-2 p-6 bg-white shadow-sm my-8 rounded-md border-t-0 mx-4'>
+                        <div className='flex flex-row justify-center items-center gap-4'>
+                            <img className="mask mask-circle w-12" src={user?.photoURL} alt={user?.displayName} />
+                            <input {...register("postDetails", {
+                                required: "Post Details is required"
+                            })}
+                                type="text" placeholder={`What's on your mind, ${user?.displayName} ?`} className="input input-bordered w-full max-w-xs" />
+                            {errors.postDetails && <span className='text-error'>{errors.postDetails.message}</span>}
+                        </div>
+                        <div className="divider"></div>
+
+                        <div className="flex flex-row justify-center items-center gap-4">
+                            <input
+                                {...register("image", {
+                                    required: "Image is required"
+                                })}
+                                type="file" className="file-input file-input-bordered file-input-primary w-64" placeholder='Upload a Image' />
+                            {errors.img && <span className='text-error'>{errors.img.message}</span>}
+                            <button className='btn btn-primary text-white'>Post</button>
+                        </div>
+
+                    </div>
+                </form>
+            }
         </div>
     );
 };
